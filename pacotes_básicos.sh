4372d8f4e4fe153f30749b480bd69c6950dd5143
JOaomoTAa17319 <67338893+JOaomoTAa17319@users.noreply.github.com>
2020-11-05 20:14:25 +0000

    Create pacotes_básicos.sh

---
+++ pacotes_básicos.sh
@@ -0,0 +1,4 @@
+# Programas Base - WAVE os SC 2.004
+# bin/sh
+sudo apt-get install snap xorg gdm3 gnome-bacgrounds gnome-session breeze-icon-theme gnome-themes-standard software-properties-gtk network-manager pulseaudio gnome-control-center gnome-system-monitor gnome-terminal gnome-calculator gnome-screenshot evince pluma totem eog nemo gnome-sudoku gnome-mines gnome-mahjongg gnome-taquin libreoffice
+snap install epiphany
